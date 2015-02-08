9b4de45cda90f73ab4ca27162e7c6a8665be160d
Jeremy Olliver <jeremy.olliver@gmail.com>
2015-02-09 12:05:46 +1300

    Fixing style violations from cane

--- lib/kitchen/provisioner/chef_extended_attributes.rb
+++ lib/kitchen/provisioner/chef_extended_attributes.rb
@@ -4,32 +4,31 @@ require 'json'
 module Kitchen
   module Provisioner
 
+    # Define a new test-kitchen provisioner, based on the chef-solo provisioner
+    # with an additional load step to insert our pre-defined / generated
+    # dna.json file before the chef run begins
     class ChefExtendedAttributes < Kitchen::Provisioner::ChefSolo
 
       default_config :merge_attributes, true
 
-      # At the time of this hook being run, `sandbox_path` has been created, populated,
-      # but not yet copied across to the destination environment
+      # At the time of this hook being run, `sandbox_path` has been created,
+      # populated, but not yet copied across to the destination environment
       def create_sandbox
         super
-        debug('chef_extended_attributes hooking create_sandbox')
+        debug('hooking create_sandbox')
         backup_original_dna_json
         if config[:merge_attributes]
           prepare_merged_json
         else
           prepare_overridden_json
         end
-        debug('chef_solo_with_attributes finished hooks')
+        debug('finished hooks')
       end
 
-      def backup_original_dna_json
-        `cp #{sandbox_path}/dna.json #{sandbox_path}/original-dna.json`
-      end
+      private
 
       def prepare_merged_json
-        info("chef_solo_with_attributes merging #{instance.suite.name}.json and kitchen attributes to dna.json")
-        source_file = "#{config[:nodes_path]}/#{instance.suite.name}.json"
-        destination_file = File.join(sandbox_path, 'dna.json')
+        info("merging #{instance.suite.name}.json and kitchen attributes")
 
         custom_json = JSON.parse(File.read(source_file))
         merged_json = custom_json.merge(config[:attributes])
@@ -38,15 +37,33 @@ module Kitchen
         File.open(destination_file, "wb") do |file|
           file.write(merged_json.to_json)
         end
-        debug('chef_solo_with_attributes merged.json written')
+        debug('merged dna.json written')
       end
 
       def prepare_overridden_json
-        info("chef_solo_with_attributes copying #{instance.suite.name}.json to dna.json")
-        source_file = "#{config[:nodes_path]}/#{instance.suite.name}.json"
-        destination_file = File.join(sandbox_path, 'dna.json')
+        info("copying #{instance.suite.name}.json to dna.json")
 
         `cp -f #{source_file} #{destination_file}`
+      end
+
+      def backup_original_dna_json
+        `cp #{sandbox_path}/dna.json #{sandbox_path}/original-dna.json`
+      end
+
+      def source_file
+        "#{config[:nodes_path]}/#{instance.suite.name}.json"
+      end
+
+      def destination_file
+        File.join(sandbox_path, 'dna.json')
+      end
+
+      def info(msg)
+        super("chef_solo_with_attributes: #{msg}")
+      end
+
+      def debug(msg)
+        super("chef_solo_with_attributes: #{msg}")
       end
 
     end
