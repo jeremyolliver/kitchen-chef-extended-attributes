21de27654368c8ce58462b9df8877b4d8ea53df4
Jeremy Olliver <jeremy.olliver@gmail.com>
2015-02-12 12:33:01 +1300

    Fix test syntax error

--- test/cookbooks/extended_attributes_test/files/default/test/kitchenoverride_test.rb
+++ test/cookbooks/extended_attributes_test/files/default/test/kitchenoverride_test.rb
@@ -2,7 +2,7 @@ require 'minitest/spec'
 
 describe_recipe 'extended_attributes_test::kitchenoverride' do
   it 'should not contain attributes from kitchen.yml' do
-    file('/tmp/kitchen/dna.json').must_not_match 'shouldnevershowup'
+    file('/tmp/kitchen/dna.json').wont_match 'shouldnevershowup'
   end
 
   it 'should include attributes from dna JSON file' do
